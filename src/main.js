00b4435b4629954ac74b0ffccd4d72cf88552504
Ahnna Östling <ahnnaostling@gmail.com>
2019-11-28 15:06:05 +0100

    add more bootstrap config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BoostrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './custom.scss'
+
+
+Vue.use(BoostrapVue)
 
 Vue.config.productionTip = false
 
