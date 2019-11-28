7387a9eb0ec762d78254d2393ad24d341de8036b
Ahnna Östling <ahnnaostling@gmail.com>
2019-11-28 11:28:03 +0100

    test component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ClientLogin msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClientLogin from './components/ClientLogin.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ClientLogin
   }
 }
 </script>
---
+++ src/components/ClientLogin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+        <img src="./assets/logo.png">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .log{
+        background-color: blue;
+    }
+</style>
