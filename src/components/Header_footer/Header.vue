8432cefe0953657903c3d93db4118e79edd0ee38
Ahnna Östling <ahnnaostling@gmail.com>
2019-12-12 16:28:48 +0100

    start code to add companyName from login

--- src/components/Header_footer/Header.vue
+++ src/components/Header_footer/Header.vue
@@ -2,7 +2,8 @@
     <div class="header">
         <header>
         <nav>
-            <span><router-link to="/" tag="span" active-class="active" exact>Login</router-link></span>
+            <h1>{ companyName }</h1>
+            <span><router-link to="/" tag="span" active-class="active" exact>Logout</router-link></span>
             <span><router-link to="/ClientCampaigns/{ companyName }" tag="span" active-class="active" exact>Home</router-link></span>
                 
         </nav>
@@ -13,6 +14,9 @@
 <style>
 nav {
   float: right;
+}
+h1 {
+  float: left;
 }
  
 nav .active {
@@ -29,7 +33,7 @@ header {
   background: #F74902;
   border-bottom: 4px solid #607d8b;
   box-sizing: border-box;
-  padding: 30px;
+  padding: 50px;
   color: #ffffff;
   font-size: 30px;
 }
